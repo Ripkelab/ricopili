2ea0eb0557cb33521fed970647078f6f8dc3ea59
Robert Karlsson <robkar@gmail.com>
2015-12-02 22:56:34 +0100

    Extract configuration file reading to Ricopili::Utils

---
+++ rp_bin/Ricopili/Utils.pm
@@ -0,0 +1,36 @@
+package Ricopili::Utils;
+
+use strict;
+use warnings;
+
+BEGIN {
+    require Exporter;
+    our @ISA         = qw(Exporter);
+    our @EXPORT_OK   = qw(trans $conf_file);
+}
+
+#############################
+# read config file
+#############################
+
+our $conf_file = $ENV{HOME}."/ricopili.conf";
+my %conf = ();
+
+die $!."($conf_file)" unless open FILE, "< $conf_file";
+while (my $line = <FILE>){
+    next if ($line =~ /^#/);
+    my @cells = split /\s+/, $line;
+    next unless ($#cells >= 1);
+    $conf{$cells[0]} = $cells[1];
+}
+close FILE;
+
+sub trans {
+    my ($expr) = @_;
+    unless (exists $conf{$expr}) {
+	     die "config file without entry: $expr\n";
+    }
+    $conf{$expr};
+}
+
+1;
