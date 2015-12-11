5fe6e35a718f2b6a2a3fc1cc1b6c25ea055573f9
Robert Karlsson <robkar@gmail.com>
2015-12-11 15:24:00 +0100

    Substitute environment variables in config file at runtime

--- rp_bin/Ricopili/Utils.pm
+++ rp_bin/Ricopili/Utils.pm
@@ -21,6 +21,12 @@ while (my $line = <FILE>){
     next if ($line =~ /^#/);
     my @cells = split /\s+/, $line;
     next unless ($#cells >= 1);
+
+    # expand environment variables and '~' for home directory in conf entries
+    $cells[1] =~ s/^~/$ENV{HOME}/;
+    $cells[1] =~ s/\$\{(\w+)\}/$ENV{$1}/g;
+    $cells[1] =~ s/\$(\w+)/$ENV{$1}/g;
+
     $conf{$cells[0]} = $cells[1];
 }
 close FILE;
